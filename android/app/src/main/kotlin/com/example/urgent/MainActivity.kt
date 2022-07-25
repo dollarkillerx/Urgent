b7bcb986182fc80280469f48262eb8a4b0ca1397
dollarkillerx <adapawang@gmail.com>
2022-07-25 19:22:55 +0800

    init project

---
+++ android/app/src/main/kotlin/com/example/urgent/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.urgent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:urgent/common/routes/app_pages.dart';
+import 'package:urgent/theme/app_themes.dart';
+
+void main() {
+  // 设置沉浸式状态栏
+  if(Platform.isAndroid){
+    SystemUiOverlayStyle style = SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light
+    );
+    SystemChrome.setSystemUIOverlayStyle(style);
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Urgent Manager",
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppPages.INIT,
+      getPages: AppPages.routes,
+      unknownRoute: AppPages.unknownRoute,
+      debugShowMaterialGrid: false,
+      darkTheme: AppThemes.dark,
+      theme: AppThemes.light,
+      themeMode: ThemeMode.system,
+    );
+  }
+}
